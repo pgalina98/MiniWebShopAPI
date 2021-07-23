2a0ae1ffc3c6577064acd64fdc6cf13e4e11b359
Patrik Galina <pgalina@foi.hr>
2021-07-23 09:08:08 +0200

    Project initialized

---
+++ src/main/java/org/kingict/miniwebshop/MiniWebShopApplication.java
@@ -0,0 +1,13 @@
+package org.kingict.miniwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniWebShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kingict/miniwebshop/MiniWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kingict.miniwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
