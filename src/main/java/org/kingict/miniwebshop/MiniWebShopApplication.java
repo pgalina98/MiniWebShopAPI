f5d99f4e4181318e9d1b23367a55d2d74b08bd4c
Patrik Galina <pgalina@foi.hr>
2021-07-25 20:27:10 +0200

    Added support for Cross-Origin Resource Sharing (CORS)

--- src/main/java/org/kingict/miniwebshop/MiniWebShopApplication.java
+++ src/main/java/org/kingict/miniwebshop/MiniWebShopApplication.java
@@ -2,6 +2,11 @@ package org.kingict.miniwebshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class MiniWebShopApplication {
@@ -10,4 +15,20 @@ public class MiniWebShopApplication {
         SpringApplication.run(MiniWebShopApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterBean(){
+        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/*", configuration);
+
+        filterRegistrationBean.setFilter(new CorsFilter(source));
+        filterRegistrationBean.setOrder(0);
+
+        return filterRegistrationBean;
+    }
 }
